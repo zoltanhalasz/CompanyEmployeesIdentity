e7dbefadb7033e1188759090cbff31f1f525a1fd
Zoltan Halasz <42749923+zoltanhalasz@users.noreply.github.com>
2020-11-09 14:04:42 +0200

    Add project files.

---
+++ CompanyEmployees.Client/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CompanyEmployees.Client.Models;
+using System.Text.Json;
+using System.Net.Http;
+
+namespace CompanyEmployees.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Companies() 
+        { 
+            var httpClient = _httpClientFactory.CreateClient("APIClient"); 
+            var response = await httpClient.GetAsync("api/companies").ConfigureAwait(false); 
+            response.EnsureSuccessStatusCode();
+            var companiesString = await response.Content.ReadAsStringAsync();
+            var companies = JsonSerializer.Deserialize<List<CompanyViewModel>>(companiesString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }); 
+            return View(companies); 
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CompanyEmployees.IDP/Config.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace CompanyEmployees.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<ApiResource> Apis => new ApiResource[] { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { };
+    }
+}
